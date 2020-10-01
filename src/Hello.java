271531ad81b3ccff87b547aedfeb7c120535c593
sujata swami <sujataswami12@gmail.com>
2020-10-01 16:40:21 +0530

    change statement add

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,14 @@
 public class Hello {
+
+    void add(){
+
+        System.out.println("started...");
+    }
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("welcome");
+
+        Hello h=new Hello();
+        h.add();
     }
 }
